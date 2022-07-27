c5fc8716e0dfb50a83023602c3d23bde010dfe8d
Amajuoyi chidera <Amajuoyichideradavid@gmail.com>
2022-07-27 14:52:15 -0700

    first version

--- calc.js
+++ calc.js
@@ -1,36 +1,39 @@
-let a = '';
-let b = '';
+// let a = '';
+// let b = '';
 let num = [];
-let ans;
+// let ans;
 
 // All the numbers and operators input will be stored in an array "num" using function "sendNum()"
-function sendNum(digit) {
-    num.push(digit);
+function sendNum(Digit) {
+    num.push(Digit);
     if (num.length !=1) {
-        a = '';
-        document.getElementById('screen').innerHTML = a;       // clearing the screen.  
+        // a = '';
+        document.getElementById('screen').innerHTML = '';       // clearing the screen.  
     }
 
-    for (i=0; i<num.length; i++) {
-        a = a + num[i]                                         // concatenate the elements of the array "num" into a single string, which will be displayed on the screen
-    }
-    document.getElementById('screen').innerHTML = a;          // displaying the concatenated string
+    // for (i=0; i<num.length; i++) {
+    //     a += num[i]                                         // concatenate the elements of the array "num" into a single string, which will be displayed on the screen
+    // }
+    document.getElementById('screen').innerHTML = num.join('');          // displaying the concatenated string
 }
 
 // when the user presses "=", function "equalTo()" is called
 function equalTo() {
     document.getElementById('screen').innerHTML = '';
 
-   for(i=0; i<num.length; i++) {
-       b += num[i];                          // concatenating the array "num" into a single sting
-   }
-   ans = eval(b)
+//    for(i=0; i<num.length; i++) {
+//        b += num[i];                          // concatenating the array "num" into a single sting
+//    }
+   ans = eval(num.join(''))
 
    document.getElementById('screen').innerHTML = ans;    // result display
 
-   while(num.length > 0) {
-       num.pop();                 // emptying the array "num"
-   }
+//    while(num.length > 0) {
+//        num.pop();                 // emptying the array "num"
+//    }
+
+    // num = []
+    num.length = 0
 
    num.push(ans.toString());
 
@@ -40,10 +43,13 @@ function equalTo() {
 function clearSrc() {
     document.getElementById('screen').innerHTML = '';
 
-    while(num.length > 0) {
-        num.pop();                       // emptying the array "num"
-    }
+    //x while(num.length > 0) {
+    //     num.pop();                       // emptying the array "num"
+    // }
 
-    a = '';
-    b = '';
+    // num = []
+    num.length = 0
+
+    // a = '';
+    // b = '';
 }
